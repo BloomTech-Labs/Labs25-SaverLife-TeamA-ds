3f03fa95cfe64e8b20845724a9d60ebb473b2fb0
Doug Cohen <dougscohen@gmail.com>
2020-08-18 18:35:44 -0400

    FEAT: changed one viz endpoint to POST request

--- project/app/api/viz.py
+++ project/app/api/viz.py
@@ -1,10 +1,33 @@
-from fastapi import APIRouter, HTTPException
-import pandas as pd
+import logging
 import plotly.express as px
+import pandas as pd
+
+from fastapi import APIRouter, HTTPException
 from app.helpers import *
 from app.user import User
+from pydantic import BaseModel, Field, validator
 
+log = logging.getLogger(__name__)
 router = APIRouter()
+
+class Item(BaseModel):
+    """Use this data model to parse the request body JSON."""
+    user_ID: str = Field(..., example='1635ob1dkQIz1QMjLmBpt0E36VyM96ImeyrgZ')
+    graph_type: str = Field(..., example='pie')
+    place_holder1: int = Field(..., example=42)
+    def to_df(self):
+        """Convert pydantic object to pandas dataframe with 1 row."""
+        return pd.DataFrame([dict(self)])
+    def to_dict(self):
+        """Convert pydantic object to python dictionary."""
+        return dict(self)
+    @validator('user_ID')
+    def user_ID_must_exist(cls, value):
+        """Validate that user_id is a valid ID."""
+        # load sample data and create a set of the user ID's
+        users = set(clean_data()['plaid_account_id'])
+        assert value in users, f'the user_ID {value} is invalid'
+        return value
 
 
 @router.get('/viz/{statecode}')
@@ -78,21 +101,27 @@ async def moneyflow(user_id: str):
     user = User(user_id, transactions)
     return user.money_flow()
 
-@router.get('/{user_id}/spending/{graph_type}')
-async def spending(user_id: str, graph_type: str):
-    """
-    Visualize a user's spending history by category
-    
-    ### Path Parameter
-    `user_id`: The unique plaid_account_id of a user
-    
-    `graph_type`: pie or bar
 
+@router.post('/spending_post')
+async def spending_post(item: Item):
+    """
+    Make random baseline predictions for classification problem 🔮
+    ### Request Body
+    - `User_ID`: str
+    - `graph_type`: str
+    - `place_holder_input`: int
     ### Response
-    JSON string to render with [react-plotly.js](https://plotly.com/javascript/react/) 
-    """ 
-    transactions = clean_data()
+    - `plotly object`:
+    visualizing the user's spending habits in the form of the selected graph
+    type.
+    """
+    # Get the JSON object from the POST request body and cast it to a python dictionary
+    input_dict = item.to_dict()
+    user_id = input_dict['user_ID']
+    graph_type = input_dict['graph_type']
     
+    ### Everything below is copy and pasted code from the spending() function in viz.py
+    transactions = clean_data()
     unique_users = set(transactions['plaid_account_id'].unique())
     
     # Validate the user
@@ -100,7 +129,9 @@ async def spending(user_id: str, graph_type: str):
         raise HTTPException(status_code=404, detail=f'User {user_id} not found')
     
     user = User(user_id, transactions)
+    
     if graph_type == 'pie':
         return user.categorical_spending()
+    
     if graph_type == 'bar':
         return user.bar_viz()
